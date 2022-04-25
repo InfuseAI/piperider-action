230352d21f3c60c555c325bbfbc3f23140a0de06
Kent Huang <kentwelcome@gmail.com>
2022-04-25 18:20:41 +0800

    [Feature] sc-25552 upgrade to use pipeline-cli v0.1.1
     - Add GITHUB_ACTION_URL as external_url in metadata
     - Add --one-json
     - Set the Piperider API server URL
    
    Signed-off-by: Kent Huang <kentwelcome@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,9 @@
 set -o pipefail
 
 GITHUB_ACTION_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
+export PIPERIDER_SERVICE_URL=https://api.piperider.io/v1
 
-eval "piperider-cli run $1" | tee output.log ; rc=$?
+eval "piperider-cli run $1 --one-json --metadata external_url=${GITHUB_ACTION_URL}" | tee output.log ; rc=$?
 
 pushd /usr/src/github-action
 /root/.nvm/versions/node/v16.13.0/bin/node index.js $rc || exit $?
