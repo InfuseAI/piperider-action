dce6308f5c27ffbd7bdf9941bfb3743b1f14027c
Wei-Chun, Chang <wcchang@infuseai.io>
2022-06-07 16:09:00 +0800

    add file check and remove redundant code
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,13 @@ set -o pipefail
 
 export GITHUB_ACTION_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
-eval "pip install --no-cache-dir -r ${GITHUB_WORKSPACE}/requirements.txt"
-eval piperider-cli run --no-interaction --generate-report | tee output.log ; rc=$?
+if [ ! -f ${GITHUB_WORKSPACE}/requirements.txt ]; then
+    echo "requirements.txt not found!"
+    exit 1
+fi
+
+pip install --no-cache-dir -r ${GITHUB_WORKSPACE}/requirements.txt
+piperider-cli run --no-interaction --generate-report | tee output.log ; rc=$?
 
 pushd /usr/src/github-action
 /root/.nvm/versions/node/v16.13.0/bin/node index.js $rc || exit $?
