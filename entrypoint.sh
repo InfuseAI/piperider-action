e65f56b4b86c375e4748caf2f8c56559f86d0e99
Wei-Chun, Chang <wcchang@infuseai.io>
2022-07-08 17:40:23 +0800

    change piperider user_id path
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -o pipefail
 export GITHUB_ACTION_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
 uuid=$(uuidgen -n @oid -N "${GITHUB_REPOSITORY}" --sha1 | tr -d "-")
-sed -i "s/^user_id: .*$/user_id: ${uuid}/" ${GITHUB_WORKSPACE}/.piperider/profile.yml
+sed -i "s/^user_id: .*$/user_id: ${uuid}/" ~/.piperider/profile.yml
 
 if [ -f ${GITHUB_WORKSPACE}/requirements.txt ]; then
     pip install --no-cache-dir -r ${GITHUB_WORKSPACE}/requirements.txt
