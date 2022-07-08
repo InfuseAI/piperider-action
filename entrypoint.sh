97ffea6565bfd41b6bdb62a9cb23d7811c938908
Wei-Chun, Chang <wcchang@infuseai.io>
2022-07-08 17:11:17 +0800

    gnerate uuid with uuidgen sha1
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 export GITHUB_ACTION_URL="https://github.com/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}"
 
-uuid=$(md5 -q -s ${GITHUB_REPOSITORY})
+uuid=$(uuidgen -n @oid -N "${GITHUB_REPOSITORY}" --sha1 | tr -d "-")
 sed -i "s/^user_id: .*$/user_id: ${uuid}/" ${GITHUB_WORKSPACE}/.piperider/profile.yml
 
 if [ -f ${GITHUB_WORKSPACE}/requirements.txt ]; then
@@ -15,6 +15,7 @@ piperider run --no-interaction --dbt-test | tee output.log ; rc=$?
 log=$(cat output.log)
 echo "::set-output name=analysis::${log}"
 echo "::set-output name=status::${rc}"
+echo "::set-output name=uuid::${uuid}"
 
 pushd /usr/src/github-action
 /root/.nvm/versions/node/v16.13.0/bin/node index.js $rc || exit $?
